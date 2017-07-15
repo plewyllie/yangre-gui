afce4ee020159a3735f50bf792895c882098e147
Pieter Lewyllie <plewyllie@gmail.com>
2017-07-15 22:44:06 +0200

    multiple patterns + w3cgre match

--- app/views.py
+++ app/views.py
@@ -17,9 +17,24 @@ def yangre():
         #f.write(str(subprocess.run(["./yangre", "1"]).returncode))
     #print (config.YANGGRE_PATH + " -p " + "\"" + str(request.form['pattern']) +  "\" " + "\""+ str(request.form['content'] + "\""));
     #return jsonify({'result': "1"})
-    print(config.YANGGRE_PATH,
-                       " -p ", "\"" + str(request.form['pattern']) +  "\" ",
-                       "\""+ str(request.form['content'] + "\""))
-    return jsonify({'result': subprocess.run([config.YANGGRE_PATH,
-                       "-p", "\"" + str(request.form['pattern']) +  "\"",
-                       "\""+ str(request.form['content'] + "\"")]).returncode });
+
+    with open("w3c_input", "w") as f: # writing the test string to file, as required by w3cgrep
+        f.write(request.form['content'])
+        f.write("\n")
+
+    #print(config.W3CGREP_PATH,str(request.form['pattern']),"w3c_input")
+    if not subprocess.run([config.W3CGREP_PATH,str(request.form['pattern']),"w3c_input"], stdout=subprocess.PIPE).stdout:
+        print("empty!")
+    print("stdout" + str(subprocess.run([config.W3CGREP_PATH,str(request.form['pattern']),"w3c_input"], stdout=subprocess.PIPE).stdout))
+
+    w3c_input_obj = subprocess.run([config.W3CGREP_PATH,str(request.form['pattern']),"w3c_input"], stdout=subprocess.PIPE)
+    if not w3c_input_obj.stdout:
+        w3c_input_result = 0
+    else:
+        w3c_input_result = 1
+
+    return jsonify({'w3cgrep_result' : w3c_input_result,
+                    'yangre_result' : 0});
+    #return jsonify({'result': subprocess.run([config.YANGGRE_PATH,
+    #                   "-p", "\"" + str(request.form['pattern']) +  "\"",
+    #                   "\""+ str(request.form['content'] + "\"")]).returncode });
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 YANGGRE_PATH = "/home/pilewyll/devbranch/libyang/build/yangre"
+W3CGREP_PATH = "/Users/pilewyll/yangre-gui/w3cgrep"
