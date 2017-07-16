ec0e00e08b2a7c4a566f3fbf070919ab46cf3b72
Pieter Lewyllie <plewyllie@gmail.com>
2017-07-16 09:36:17 +0200

    adding output to JSON

--- app/views.py
+++ app/views.py
@@ -22,15 +22,20 @@ def yangre():
         f.write(request.form['content'])
         f.write("\n")
 
-    w3c_input_obj = subprocess.run([config.W3CGREP_PATH,str(request.form['pattern']),"w3c_input"], stdout=subprocess.PIPE)
+    w3c_input_obj = subprocess.run([config.W3CGREP_PATH,str(request.form['pattern']),"w3c_input"],
+                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT);
     if not w3c_input_obj.stdout:
         w3c_input_result = 1
     else:
         w3c_input_result = 0
 
+    yangre_input_obj = subprocess.run([config.YANGGRE_PATH, "-p", str(request.form['pattern']),
+                                      str(request.form['content'])], stdout=subprocess.PIPE, stderr=subprocess.STDOUT);
+
+
     print (config.YANGGRE_PATH, "-p", str(request.form['pattern']), "\""+ str(request.form['content'] + "\""))
 
     return jsonify({'w3cgrep_result' : w3c_input_result,
-                    'yangre_result' : subprocess.run([config.YANGGRE_PATH,
-                       "-p", str(request.form['pattern']),
-                       str(request.form['content'])]).returncode });
+                    'w3cgrep_output' : w3c_input_obj.stdout,
+                    'yangre_result' : yangre_input_obj.returncode,
+                    'yangre_output': yangre_input_obj.stdout });
