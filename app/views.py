ebff3261dbe9e459222933b6ad8915577d718b68
Pieter Lewyllie <plewyllie@gmail.com>
2017-07-16 16:23:12 +0200

    debug

--- app/views.py
+++ app/views.py
@@ -23,7 +23,8 @@ def yangre():
     if not w3c_input_obj.stdout:
         w3c_input_result = 1
     else:
-        print("RETURN CODE W3C", w3c_input_obj.returncode)
+        if w3c_input_obj.returncode == 1:
+            w3c_input_result = -1 # I used -1 as error code
         w3c_input_result = 0
 
     yangre_input_obj = {}
