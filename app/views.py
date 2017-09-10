c9fc0c10fc02fc0deb22e84d7d35c352e16bb65a
Pieter Lewyllie <plewyllie@gmail.com>
2017-09-10 17:28:52 +0200

    applying pylint

--- app/views.py
+++ app/views.py
@@ -1,33 +1,40 @@
-from flask import render_template, flash, redirect, jsonify, request
-from app import app
 import subprocess
 import config
+from flask import render_template, jsonify, request
+from app import app
 
-@app.route('/', methods=['GET','POST'])
-@app.route('/index', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
 
-@app.route('/yangre', methods=['GET','POST'])
+
+@app.route('/yangre', methods=['GET', 'POST'])
 def yangre():
-    with open("w3c_input", "w") as f: # writing the test string to file, as required by w3cgrep
-        f.write(request.form['content'])
-        f.write("\n")
+    # writing the test string to file, as required by w3cgrep
+    with open("w3c_input", "w") as testfile:
+        testfile.write(request.form['content'])
+        testfile.write("\n")
 
     # An unquoted string is any sequence of characters that does not
     # contain any space, tab, carriage return, or line feed characters, a
     # single or double quote character, a semicolon (";"), braces ("{" or
     # "}"), or comment sequences ("//", "/*", or "*/").
-    
 
     # python 3.5 dependency. To get stdout as a string we need the universal_newlines=True parameter
     # in python 3.6 this changes to encoding='utf8'
-    w3c_input_obj = subprocess.run([config.W3CGREP_PATH,str(request.form['pattern']),"w3c_input"],
-                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True);
+    w3c_input_obj = subprocess.run(
+        [config.W3CGREP_PATH,
+         str(request.form['pattern']), "w3c_input"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True)
     if not w3c_input_obj.stdout:
         w3c_input_result = 1
     else:
@@ -35,26 +42,41 @@ def yangre():
 
     yangre_input_obj = {}
     if request.form['inverted'] == "true":
-        w3c_input_result = int(not(w3c_input_result))
+        w3c_input_result = int(not w3c_input_result)
 
         # python 3.5 dependency. To get stdout as a string we need the universal_newlines=True parameter
         # in python 3.6 this changes to encoding='utf8'
-        yangre_input_obj = subprocess.run([config.YANGGRE_PATH, "-p", str(request.form['pattern']), "-i",
-                                          str(request.form['content'])],
-                                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True);
+        yangre_input_obj = subprocess.run(
+            [
+                config.YANGGRE_PATH, "-p",
+                str(request.form['pattern']), "-i",
+                str(request.form['content'])
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True)
     else:
         # python 3.5 dependency. To get stdout as a string we need the universal_newlines=True parameter
         # in python 3.6 this changes to encoding='utf8'
-        print("Passing", config.YANGGRE_PATH, "-p", str(request.form['pattern']), str(request.form['content']))
-        yangre_input_obj = subprocess.run([config.YANGGRE_PATH, "-p", str(request.form['pattern']),
-                                          str(request.form['content'])],
-                                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True);
+        print("Passing", config.YANGGRE_PATH, "-p",
+              str(request.form['pattern']), str(request.form['content']))
+        yangre_input_obj = subprocess.run(
+            [
+                config.YANGGRE_PATH, "-p",
+                str(request.form['pattern']),
+                str(request.form['content'])
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True)
 
     if w3c_input_obj.returncode == 1:
-        w3c_input_result = -1 # I used -1 as error code
+        w3c_input_result = -1  # I used -1 as error code
 
-    return jsonify({'pattern_nb' : request.form['pattern_nb'],
-                    'w3cgrep_result' : w3c_input_result,
-                    'w3cgrep_output' : w3c_input_obj.stdout,
-                    'yangre_result' : yangre_input_obj.returncode,
-                    'yangre_output': yangre_input_obj.stdout });
+    return jsonify({
+        'pattern_nb': request.form['pattern_nb'],
+        'w3cgrep_result': w3c_input_result,
+        'w3cgrep_output': w3c_input_obj.stdout,
+        'yangre_result': yangre_input_obj.returncode,
+        'yangre_output': yangre_input_obj.stdout
+    })
