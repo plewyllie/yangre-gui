7327f356c29e492fdc16c7aa2838795922e74b55
Pieter Lewyllie <plewyllie@gmail.com>
2017-10-08 15:54:16 +0200

    split API

--- app/views.py
+++ app/views.py
@@ -23,20 +23,13 @@ def w3cgrep():  # loads the w3cgrep validator
         'w3cgrep.html', title="W3C Regex Expression Validator")
 
 
-@app.route(config.PREFIX + '/yangre', methods=['GET', 'POST'])
-def yangre():
+@app.route(config.PREFIX + '/w3c', methods=['GET', 'POST'])
+def w3c():  # JSON API to validate W3C input
     # writing the test string to file, as required by w3cgrep
     w3cinput_filename = "w3c_input" + request.form['pattern_nb']
     with open(w3cinput_filename, "w") as testfile:
         testfile.write(request.form['content'])
         testfile.write("\n")
-
-    # writing the test string to another file for yangre
-    yangreinput_filename = "yangre_input" + request.form['pattern_nb']
-    with open(yangreinput_filename, "w") as yangrefile:
-        yangrefile.write(str(request.form['pattern']))
-        yangrefile.write("\n\n")
-        yangrefile.write(str(request.form['content']))
 
     # python 3.5 dependency. To get stdout as a string we need the universal_newlines=True parameter
     # in python 3.6 this changes to encoding='utf8'
@@ -51,10 +44,33 @@ def yangre():
     else:
         w3c_input_result = 0
 
-    yangre_input_obj = {}
     if request.form['inverted'] == "true":
         w3c_input_result = int(not w3c_input_result)
 
+    if w3c_input_obj.returncode == 1:
+        w3c_input_result = -1  # I used -1 as error code
+
+    # clean up files
+    os.remove(w3cinput_filename)
+
+    return jsonify({
+        'pattern_nb': request.form['pattern_nb'],
+        'w3cgrep_result': w3c_input_result,
+        'w3cgrep_output': w3c_input_obj.stdout
+    })
+
+
+@app.route(config.PREFIX + '/yangre', methods=['GET', 'POST'])
+def yangre():  # JSON API to validate YANG input
+    # writing the test string to another file for yangre
+    yangreinput_filename = "yangre_input" + request.form['pattern_nb']
+    with open(yangreinput_filename, "w") as yangrefile:
+        yangrefile.write(str(request.form['pattern']))
+        yangrefile.write("\n\n")
+        yangrefile.write(str(request.form['content']))
+
+    yangre_input_obj = {}
+    if request.form['inverted'] == "true":
         # python 3.5 dependency. To get stdout as a string we need the universal_newlines=True parameter
         # in python 3.6 this changes to encoding='utf8'
         yangre_input_obj = subprocess.run(
@@ -71,17 +87,11 @@ def yangre():
             stderr=subprocess.STDOUT,
             universal_newlines=True)
 
-    if w3c_input_obj.returncode == 1:
-        w3c_input_result = -1  # I used -1 as error code
-
     # clean up files
-    os.remove(w3cinput_filename)
     os.remove(yangreinput_filename)
 
     return jsonify({
         'pattern_nb': request.form['pattern_nb'],
-        'w3cgrep_result': w3c_input_result,
-        'w3cgrep_output': w3c_input_obj.stdout,
         'yangre_result': yangre_input_obj.returncode,
         'yangre_output': yangre_input_obj.stdout
     })
