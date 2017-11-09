f04083374898ae0bb97513cc74e316172028418f
Pieter Lewyllie <plewyllie@gmail.com>
2017-11-09 19:47:21 +0100

    test environ

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
+os.environ['LD_LIBRARY_PATH'] = "/usr/local/lib64"
+
 YANGGRE_PATH = "/home/pilewyll/libyang-prod/build/yangre"
 #W3CGREP_PATH = "/Users/pilewyll/yangre-gui/w3cgrep"
 W3CGREP_PATH = "/home/pilewyll/w3cgrep"
 PREFIX = "/yangre"  # changing this will break things, as it is also used in ajax calls
 APIPREFIX = "/yangre/v1"
-
-os.environ['LD_LIBRARY_PATH'] = "/usr/local/lib64"
