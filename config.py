5cf4fe7731fe3503bdc9b3a91b50023a24287083
Pieter Lewyllie <plewyllie@gmail.com>
2017-11-09 19:39:51 +0100

    new yangre

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-YANGGRE_PATH = "/home/pilewyll/devbranch/libyang/build/yangre"
+YANGGRE_PATH = "/home/pilewyll/libyang-prod/build/yangre"
 #W3CGREP_PATH = "/Users/pilewyll/yangre-gui/w3cgrep"
 W3CGREP_PATH = "/home/pilewyll/w3cgrep"
 PREFIX = "/yangre"  # changing this will break things, as it is also used in ajax calls
